3257b6714c4a58db6ac48422df6ebd60a64e551e
Dean Hiller <dhiller@twitter.com>
2018-06-13 10:12:57 -0600

    fix this

--- src/main/java/com/duprasville/limiters/DistributedRateLimiters.java
+++
@@ -1,6 +0,0 @@
-package com.duprasville.limiters;
-
-public class DistributedRateLimiters {
-    public static final DistributedRateLimiter UNLIMITED = (permits) -> true;
-    public static final DistributedRateLimiter NEVER = (permits) -> false;
-}
