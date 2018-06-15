664b53a77cb76958200f802f73e09bdfd0daaf59
Michael Hirsch <mhirsch@twitter.com>
2018-06-15 15:07:04 -0600

    Only send to the row above, not to any node with a smaller id.

--- src/main/java/com/duprasville/limiters/treefill/WindowStateWithRandomizedRedirects.java
+++ src/main/java/com/duprasville/limiters/treefill/WindowStateWithRandomizedRedirects.java
@@ -8,10 +8,23 @@ import com.duprasville.limiters.treefill.domain.Detect;
 public class WindowStateWithRandomizedRedirects extends WindowState {
 
   private final Random random;
+  private final long LimitForGraphAboveWindowState;
 
   public WindowStateWithRandomizedRedirects(long id, long N, long W, MessageSender m) {
     super(id, N, W, m);
     random = new Random();
+    int row = computeRowOfWindowStateFromId(id);
+    LimitForGraphAboveWindowState = (long) Math.pow(2, row);
+  }
+
+  // zero indexed row
+  private int computeRowOfWindowStateFromId(long id) {
+    if (id == 1) return 0;
+    int i = 0;
+    while ( Math.pow(2,i) <= id ) {
+      i++;
+    }
+    return i-1;
   }
 
   @Override
@@ -20,7 +33,7 @@ public class WindowStateWithRandomizedRedirects extends WindowState {
    * random node higher in the tree
    */
   protected void redirectDetectMessage(Detect detectMesage) {
-    long dest = random.nextInt((int) this.id-1) + 1;
+    long dest = random.nextInt((int) LimitForGraphAboveWindowState) + 1;
     messageSender.send(  // Sending to parent
         new Detect(
             detectMesage.getSrc(),
